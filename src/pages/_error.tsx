2eba13699358dd46d30e7373ba3361983f796586
ghmasood <msd.drg@gmail.com>
2023-12-11 16:28:28 +0330

    [ADD] error page

---
+++ src/pages/_error.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { useState } from "react";
+import { NextPage, NextPageContext } from "next";
+import { useRouter } from "next/router";
+
+import styles from "./error.module.scss";
+import { RefreshCircle } from "iconsax-react";
+interface Props {
+  statusCode?: number;
+}
+
+const Error: NextPage<Props> = ({ statusCode }) => {
+  //ROUTER
+  const router = useRouter();
+  //STATES
+  const [loading, setLoading] = useState(false);
+  return (
+    <div className={styles.errorPage}>
+      <p>{statusCode}</p>
+      <div>
+        {statusCode === 404
+          ? "متاسفانه گم شده اید :("
+          : "متاسفانه مشکلی رخ داده است :("}
+      </div>
+      <span
+        onClick={() => {
+          setLoading(true);
+          router.replace("/?limit=12");
+        }}
+      >
+        {loading ? (
+          <RefreshCircle className="loading" size="2rem" />
+        ) : (
+          "بازگشت به خانه؟"
+        )}
+      </span>
+    </div>
+  );
+};
+
+Error.getInitialProps = ({ res, err }: NextPageContext) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  return { statusCode };
+};
+
+export default Error;
